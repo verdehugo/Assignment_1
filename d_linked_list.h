14676008953f5a903cdef71f411d7e6bbcaf4dd3
Hugo Verde <verde.hugo@ua.pt>
2024-03-08 17:10:38 +0000

    Updates Ficheiro .h

--- d_linked_list.h
+++ d_linked_list.h
@@ -1,9 +1,11 @@
+#ifndef D_LINKED_LIST_H
+#define D_LINKED_LIST_H
 #include<stdio.h>
-#include<stdli.h>
+#include<stdlib.h>
 #include<stdint.h>
 
 #define MAX_LL_SIZE 100
-typedef struct{
+typedef struct Node{
 
 	uint16_t id;
 	unsigned char data [MAX_LL_SIZE];
@@ -13,6 +15,7 @@ typedef struct{
 } Node;
 
 typedef struct{
+	Node nodes[MAX_LL_SIZE];	// Array of nodes
 
 	Node* head;
 	Node* tail;
@@ -22,16 +25,16 @@ typedef struct{
 	
 } DoublyLinkedList;
 
-void MyDLLInit(DoublyLinkedList *dll, size_t max_elem, size_t elem_size);
+void MYDLLInit(DoublyLinkedList *dll, size_t max_elem, size_t elem_size);
 
-void MyDLLInsert(DoubleLinkedList* dll, uint16_t key, unsigned char*data);	
+void MYDLLInsert(DoublyLinkedList* dll, uint16_t key, unsigned char*data);	
 
-void MyDLLRemove(DoubleLinkedList* dll, uint16_t key);
+void MYDLLRemove(DoublyLinkedList* dll, uint16_t key);
 
-void MyDLLFind(DoubleLinkedList* dll, uint16_t key, unsigned char* result);
+const unsigned char* MYDLLFind(DoublyLinkedList* dll, uint16_t key);
 
-void MyDLLFindNext(DoublyLinkedList* dll, uint16_t key, unsigned char*result);
+const unsigned char* MYDLLFindNext(DoublyLinkedList* dll, uint16_t key);
 
-void MyDLLFindPrevious(DoublyLinkedList* dll, uint16_t key, unsigned char*result);
+const unsigned char*MYDLLFindPrev(DoublyLinkedList* dll, uint16_t key);
 
-void MyDLLCleanup(DoubleLinkedList* dll);
+#endif //D_LINKED_LIST_H
