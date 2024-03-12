56295f70061cab40d264cc2a563d4fba0ba16ef7
Hugo Verde <verde.hugo@ua.pt>
2024-03-12 13:54:54 +0000

    Final Version MYDLLInsert.c

---
+++ src/MYDLLInsert.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include"d_linked_list.h"
+
+// Author: Hugo Alcaire Verde 72705
+// Date : 08 March 2024
+
+void MYDLLInsert(DoublyLinkedList *dll, uint16_t key, unsigned char* data)
+{
+    if (dll->current_size >= dll->max_elem)
+    {
+        printf("Doubly Linked List is full");
+        return;  // exit the function when the list is full
+    }
+
+    Node *newNode = &dll->nodes[dll->current_size];
+    newNode->id = key;
+
+    // Copy the data into the node
+    for (size_t i = 0; i < dll->elem_size; i++)
+    {
+        newNode->data[i] = data[i];
+    }
+
+    newNode->prev = NULL;
+    newNode->next = NULL;
+
+    if (dll->head == NULL) // Dll empty
+    {
+        dll->head = newNode;
+        dll->tail = newNode;
+    }
+    else
+    {
+        // Add new node to the end of the DLL (tail)
+        newNode->prev = dll->tail;
+        dll->tail->next = newNode;
+        dll->tail = newNode;
+    }
+
+    dll->current_size++;
+}
